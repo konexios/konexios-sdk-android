3d233b82acc89ad95bbcfd357730fb0d5fc440a2
osminin <osminin@mera.ru>
2016-12-26 13:11:42 +0300

    new api calls for telemetry history

---
+++ kronossdk/src/main/java/com/arrow/kronos/api/models/TelemetryEventCount.java
@@ -0,0 +1,40 @@
+package com.arrow.kronos.api.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by osminin on 12/23/2016.
+ */
+
+public final class TelemetryEventCount {
+    @SerializedName("timestamp")
+    private Long timestamp;
+    @SerializedName("telemetryCount")
+    private Integer telemetryCount;
+    @SerializedName("deviceEventCount")
+    private Integer deviceEventCount;
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getTelemetryCount() {
+        return telemetryCount;
+    }
+
+    public void setTelemetryCount(Integer telemetryCount) {
+        this.telemetryCount = telemetryCount;
+    }
+
+    public Integer getDeviceEventCount() {
+        return deviceEventCount;
+    }
+
+    public void setDeviceEventCount(Integer deviceEventCount) {
+        this.deviceEventCount = deviceEventCount;
+    }
+}
