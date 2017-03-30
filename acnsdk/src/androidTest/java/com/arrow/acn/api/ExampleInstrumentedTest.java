36a58f14c69f05c35d0aedeec9318a5688ac7cda
osminin <osminin@mera.ru>
2017-03-30 12:58:13 +0300

    preparation for unit tests

--- acnsdk/src/androidTest/java/com/arrow/acn/api/ExampleInstrumentedTest.java
+++ acnsdk/src/androidTest/java/com/arrow/acn/api/ExampleInstrumentedTest.java
@@ -31,6 +31,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.acnsdk.arrow.acnsdk", appContext.getPackageName());
+        assertEquals("com.arrow.acn.api.test", appContext.getPackageName());
     }
 }
