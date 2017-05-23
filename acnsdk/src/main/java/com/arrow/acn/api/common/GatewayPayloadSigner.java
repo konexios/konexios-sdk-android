4476b934610273055d83e55f406f46ddc2d15039
osminin <osminin@mera.ru>
2017-05-23 13:30:35 +0300

    change for KR-1679

---
+++ acnsdk/src/main/java/com/arrow/acn/api/common/GatewayPayloadSigner.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2017 Arrow Electronics, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License 2.0
+ * which accompanies this distribution, and is available at
+ * http://apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors: Arrow Electronics, Inc.
+ */
+
+package com.arrow.acn.api.common;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import timber.log.Timber;
+
+import static com.arrow.acn.api.Constants.Api.X_ARROW_VERSION_1;
+
+/**
+ * Created by osminin on 5/22/2017.
+ */
+
+public final class GatewayPayloadSigner {
+    private String secretKey;
+    private String hid;
+    private String name;
+    private boolean encrypted;
+    private String apiKey;
+    private List<String> parameters = new ArrayList<>();
+
+    private GatewayPayloadSigner(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public static GatewayPayloadSigner create(String secretKey) {
+        Timber.v("create: secretKey = %s", secretKey);
+        return new GatewayPayloadSigner(secretKey);
+    }
+
+    public GatewayPayloadSigner withHid(String hid) {
+        Timber.v("withHid: hid = %s", hid);
+        this.hid = hid;
+        return this;
+    }
+
+    public GatewayPayloadSigner withName(String name) {
+        Timber.v("withName: name = %s", name);
+        this.name = name;
+        return this;
+    }
+
+    public GatewayPayloadSigner withEncrypted(boolean encrypted) {
+        Timber.v("withEncrypted: encrypted = %s", encrypted);
+        this.encrypted = encrypted;
+        return this;
+    }
+
+    public GatewayPayloadSigner withApiKey(String apiKey) {
+        Timber.v("withApiKey: apiKey = %s", apiKey);
+        this.apiKey = apiKey;
+        return this;
+    }
+
+    public GatewayPayloadSigner withParameter(String name, String value) {
+        Timber.v("withParameter: name = %, value = %s", name, value);
+        parameters.add(String.format("%s=%s", name.toLowerCase(), value));
+        return this;
+    }
+
+    public String signV1() {
+        Timber.v("signV1: ");
+        StringBuilder stringToSign = new StringBuilder();
+        stringToSign.append(Utils.hash(buildCanonicalRequest())).append('\n');
+        stringToSign.append(apiKey).append('\n');
+        stringToSign.append(X_ARROW_VERSION_1);
+
+        String signingKey = Utils.hmacSha256Hex(X_ARROW_VERSION_1, Utils.hmacSha256Hex(apiKey, secretKey));
+
+        String signature = Utils.hmacSha256Hex(signingKey, stringToSign.toString());
+
+        return signature;
+    }
+
+    private String buildCanonicalRequest() {
+        Timber.v("buildCanonicalRequest: ");
+        StringBuilder builder = new StringBuilder();
+        builder.append(hid).append('\n').append(name).append('\n').append(encrypted).append('\n');
+
+        // append parameters
+        if (parameters.size() > 0) {
+            Collections.sort(parameters);
+            for (String p : parameters) {
+                builder.append(p).append('\n');
+            }
+        }
+        return builder.toString();
+    }
+}
